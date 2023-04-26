a95985c2a4510224f5f251a3477610809e122e58
harithakh <hkhulangamuwa@gmail.com>
2023-04-26 11:40:17 +0530

    initial commit

---
+++ src/components/Camera.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+const Camera = () => {
+  const webcamRef = React.useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    // Do something with the image, e.g. save it to a file or upload it to a server
+  }, [webcamRef]);
+
+  return(
+    <>
+        <Webcam audio={false} ref={webcamRef}/>
+        <button onClick={capture}>Capture</button>
+    </>
+  );
+};
+
+export default Camera;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (<>
+        <p>Hi</p>
+    </>
+    );
+  }
+  
+  export default Header;
+  
